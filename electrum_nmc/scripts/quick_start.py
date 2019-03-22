b9fefc856206ae12879d3ca9a40a5a3776fefbf9
JeremyRand <biolizard89@gmail.com>
2019-03-22 22:58:42 +0000

    Namecoin: Rebrand datadir path in quick start script

--- electrum_nmc/scripts/quick_start.py
+++ electrum_nmc/scripts/quick_start.py
@@ -8,7 +8,7 @@ from electrum_nmc.wallet import Wallet, create_new_wallet
 from electrum_nmc.commands import Commands
 
 
-config = SimpleConfig({"testnet": True})  # to use ~/.electrum/testnet as datadir
+config = SimpleConfig({"testnet": True})  # to use ~/.electrum-nmc/testnet as datadir
 constants.set_testnet()  # to set testnet magic bytes
 daemon = Daemon(config, listen_jsonrpc=False)
 network = daemon.network

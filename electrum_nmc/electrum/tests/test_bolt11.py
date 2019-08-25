75ba3976851bf28dee8a111e5a33f9000f89ab79
JeremyRand <biolizard89@gmail.com>
2019-08-25 07:05:14 +0000

    Use SEGWIT_HRP instead of bare 'tb' in test_bolt11

--- electrum_nmc/electrum/tests/test_bolt11.py
+++ electrum_nmc/electrum/tests/test_bolt11.py
@@ -2,6 +2,7 @@ from hashlib import sha256
 from electrum.lnaddr import shorten_amount, unshorten_amount, LnAddr, lnencode, lndecode, u5_to_bitarray, bitarray_to_u5
 from decimal import Decimal
 from binascii import unhexlify, hexlify
+from electrum.constants import BitcoinTestnet
 from electrum.segwit_addr import bech32_encode, bech32_decode
 import pprint
 import unittest
@@ -59,7 +60,7 @@ class TestBolt11(unittest.TestCase):
             LnAddr(RHASH, tags=[('d', '')]),
             LnAddr(RHASH, amount=Decimal('0.001'), tags=[('d', '1 cup coffee'), ('x', 60)]),
             LnAddr(RHASH, amount=Decimal('1'), tags=[('h', longdescription)]),
-            LnAddr(RHASH, currency='tb', tags=[('f', 'mk2QpYatsKicvFVuTAQLBryyccRXMUaGHP'), ('h', longdescription)]),
+            LnAddr(RHASH, currency=BitcoinTestnet.SEGWIT_HRP, tags=[('f', 'mk2QpYatsKicvFVuTAQLBryyccRXMUaGHP'), ('h', longdescription)]),
             LnAddr(RHASH, amount=24, tags=[
                 ('r', [(unhexlify('029e03a901b85534ff1e92c43c74431f7ce72046060fcf7a95c37e148f78c77255'), unhexlify('0102030405060708'), 1, 20, 3), (unhexlify('039e03a901b85534ff1e92c43c74431f7ce72046060fcf7a95c37e148f78c77255'), unhexlify('030405060708090a'), 2, 30, 4)]), ('f', '1RustyRX2oai4EYYDpQGWvEL62BBGqN9T'), ('h', longdescription)]),
             LnAddr(RHASH, amount=24, tags=[('f', '3EktnHQD7RiAE6uzMj2ZifT9YgRrkSgzQX'), ('h', longdescription)]),
